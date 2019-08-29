d3cd4f65bcee8f3f85ebecdd3ace87204eef7e59
odungern <od@enso-managers.de>
2019-08-29 23:16:21 +0200

    bugfix

--- source/js/importAny.mod.js
+++ source/js/importAny.mod.js
@@ -192,7 +192,7 @@ modules.construct({
 			if( modules.isReady(s.name) ) {
 //				console.debug('isReady',s.id,self.format);
 				app[self.format.name].init( self.format.opts );
-				if( typeof(app[self.format.name].toSpecif)=='function' ) {
+				if( typeof(app[s.name].toSpecif)=='function' ) {
 					str += '<button id="FormatSelector-'+s.id+'" onclick="'+myFullName+'.setFormat(\''+s.id+'\')" class="btn btn-default'+(self.format.id==s.id?' active':'')+'" data-toggle="popover" title="'+s.desc+'">'+s.label+'</button>'
 				} else {
 					str += '<button disabled class="btn btn-default" data-toggle="popover" title="'+s.desc+'">'+s.label+'</button>'
