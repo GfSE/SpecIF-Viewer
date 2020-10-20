e9349bc5c98392b7a7cd9571841e4f574ba86337
Treichel <Thomas.Treichel@adesso.de>
2020-10-20 10:40:33 +0200

    added prototype model for specif

---
+++ src/model/SpecIF/Project.ts
@@ -0,0 +1,84 @@
+import { DataType } from "./DataType";
+import { SpecIfNode } from "./SpecIfNode";
+import { PropertyClass } from "./PropertyClass";
+import { Resource } from "./Resource";
+import { ResourceClass } from "./ResourceClass";
+import { SpecIfFile } from "./SpecIfFile";
+import { Statement } from "./Statement";
+import { StatementClass } from "./StatementClass";
+import { SpecIfObject } from "../../Interfaces/SpecIfObject";
+
+class project implements SpecIfObject {
+    schema: string;
+    id: string;
+    title: string;
+    createdAt: Date;
+
+    dataTypes: DataType[];
+    propertyClasses: PropertyClass[];
+    resourceClasses: ResourceClass[];
+    statementClasses: StatementClass[];
+    resources: Resource[];
+    statements: Statement[];
+    hierarchies: SpecIfNode[];
+    files: SpecIfFile[];
+
+    constructor(
+        schema: string,
+        id: string,
+        title: string,
+        createdAt: Date,
+        dataTypes: DataType[],
+        propertyClasses: PropertyClass[],
+        resourceClasses: ResourceClass[],
+        statementClasses: StatementClass[],
+        resources: Resource[],
+        statements: Statement[],
+        hierarchies: SpecIfNode[],
+        files: SpecIfFile[]
+    ) {
+        this.schema = schema;
+        this.id = id;
+        this.title = title;
+        this.createdAt = createdAt;
+        this.dataTypes = dataTypes;
+        this.propertyClasses = propertyClasses;
+        this.resourceClasses = resourceClasses;
+        this.statementClasses = statementClasses;
+        this.resources = resources;
+        this.statements = statements;
+        this.hierarchies = hierarchies;
+        this.files = files;
+    }
+    
+    extractSpecIfFromArray(specIfObjectArray: SpecIfObject[]):string {
+        return `[${specIfObjectArray.map( specIfObject => specIfObject.toSpecIF()).toString()}]`;
+    }
+    
+    toSpecIF(): string {  
+        return `{
+            "$schema":"${this.schema}",
+            "id":"${this.id}",
+            "title":"${this.title}",
+            "createdAt":"${this.createdAt}",
+            "dataTypes":${this.extractSpecIfFromArray(this.dataTypes)},
+            "propertyClasses":${this.extractSpecIfFromArray(this.propertyClasses)},
+            "resourceClasses":${this.extractSpecIfFromArray(this.resourceClasses)},
+            "statementClasses":${this.extractSpecIfFromArray(this.statementClasses)},
+            "resources":${this.extractSpecIfFromArray(this.resources)},
+            "statements":${this.extractSpecIfFromArray(this.statements)},
+            "hierarchies":${this.extractSpecIfFromArray(this.hierarchies)},
+            "files":${this.extractSpecIfFromArray(this.files)}
+        }`;
+    }
+    
+    extractRDFFromArray(specIfObjectArray: SpecIfObject[]): string {
+        return `[${specIfObjectArray.map( specIfObject => specIfObject.toRDF()).toString()}]`;
+    }
+    
+    toRDF(): string {
+        return `{
+            
+        }`;
+    }
+}
---
+++ src/model/SpecIF/Statement.ts
@@ -0,0 +1,53 @@
+import { SpecIfObject } from "../../Interfaces/SpecIfObject";
+
+export class Statement implements SpecIfObject{
+    id: string;
+    title: string;
+    description: string;
+    statementClass: string;
+    subject: string;
+    object: string;
+    changedAt: string;
+
+    constructor(
+        id: string,
+        title: string,
+        description: string,
+        statementClass: string,
+        subject: string,
+        object: string,
+        changedAt: string
+    ) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.statementClass = statementClass;
+        this.subject = subject;
+        this.object = object;
+        this.changedAt = changedAt;
+    }
+    extractSpecIfFromArray(specIfObjectArray: SpecIfObject[]): string {
+        return `[${specIfObjectArray.map( specIfObject => specIfObject.toSpecIF()).toString()}]`;
+    }
+    toSpecIF(): string {
+        return `{
+            "id": "${this.id}",
+            "title": "${this.title}",
+            "description": "${this.description}",
+            "class": "${this.statementClass}",
+            "subject": "${this.subject}",
+            "object": "${this.object}",
+            "changedAt": "${this.changedAt}"
+        }`;
+    }
+    
+    extractRDFFromArray(specIfObjectArray: SpecIfObject[]): string {
+        return `[${specIfObjectArray.map( specIfObject => specIfObject.toRDF()).toString()}]`;
+    }
+    
+    toRDF(): string {
+        return `{
+            
+        }`;
+    }
+}
