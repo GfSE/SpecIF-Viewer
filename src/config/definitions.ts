5be7ec766cad4f0ee8a03093a2bfe6ba2038d527
odungern <od@enso-managers.de>
2023-07-16 23:55:54 +0200

    2 bugfixes

--- src/config/definitions.ts
+++ src/config/definitions.ts
@@ -7,7 +7,7 @@
     .. or even better as Github issue (https://github.com/GfSE/SpecIF-Viewer/issues)
 */
 const CONFIG:any = {};
-    CONFIG.appVersion = "1.1.p.6",
+    CONFIG.appVersion = "1.1.p.7",
     CONFIG.specifVersion = "1.1";
     CONFIG.imgURL = './vendor/assets/images';
     CONFIG.QuickStartGuideEn = "https://specif.de/files/SpecIF/documents/SpecIF-Introduction.pdf";
