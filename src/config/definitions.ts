06f801d48794b2442caeb8ef58e789fcf820ffe2
odungern <od@enso-managers.de>
2024-07-18 15:07:09 +0200

    next revision

--- src/config/definitions.ts
+++ src/config/definitions.ts
@@ -7,7 +7,7 @@
     .. or even better as Github issue (https://github.com/GfSE/SpecIF-Viewer/issues)
 */
 const CONFIG:any = {};
-    CONFIG.appVersion = "1.1.s.12",
+    CONFIG.appVersion = "1.1.s.13",
     CONFIG.specifVersion = "1.1";
     CONFIG.imgURL = './assets/images';
     CONFIG.ontologyURL = 'https://specif.de/v1.1/Ontology.specif';  // used to localize and normalize terms
