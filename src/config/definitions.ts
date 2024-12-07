816ca6171656e7169553627d1666f5e73cdcae04
odungern <od@enso-managers.de>
2024-12-07 12:06:26 +0100

    Correct version number v1.2.a.2

--- src/config/definitions.ts
+++ src/config/definitions.ts
@@ -6,7 +6,7 @@
     We appreciate any correction, comment or contribution as Github issue (https://github.com/GfSE/SpecIF-Viewer/issues)
 */
 const CONFIG:any = {};
-    CONFIG.appVersion = "1.2.s.17",
+    CONFIG.appVersion = "1.2.a.2",
     CONFIG.specifVersion = "1.2";
     CONFIG.imgURL = './assets/images';
     CONFIG.ontologyURL = 'https://specif.de/v1.2/Ontology.specif';  // used to localize and normalize terms
